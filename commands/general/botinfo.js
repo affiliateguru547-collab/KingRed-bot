400b429e776a608e5c71bb6e7c283e8a6048b85f
affiliateguru547-collab <affiliateguru547@gmail.com>
2026-09-17 23:30:37 +0300

    Update botinfo.js

--- commands/general/botinfo.js
+++ commands/general/botinfo.js
@@ -9,15 +9,15 @@ module.exports = {
         const memory = (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2);
         const totalMem = (os.totalmem() / 1024 / 1024 / 1024).toFixed(2);
         
-        const info = `🤖 *FIREBOX BOT SYSTEM INFO*\n\n` +
+        const info = `🤖 *KINGRE BOT SYSTEM INFO*\n\n` +
                      `✨ *Version:* \`2.5.0 (Moderation)\`\n` +
-                     `👨‍💻 *Developer:* \`Firebox Studios\`\n` +
+                     `👨‍💻 *Developer:* \`Kingred Studios\`\n` +
                      `📂 *GitHub:* https://github.com/njogu26713-commits/firebox-bot\n` +
-                     `💬 *Support:* https://chat.whatsapp.com/E8BOikeeP9a0ds2odgneHy\n\n` +
+                     `💬 *Support:* https://whatsapp.com/channel/0029Vb9AwScF6sn47ClubG1Z\n\n` +
                      `💻 *Platform:* \`${os.platform()}\`\n` +
                      `📟 *Memory:* \`${memory}MB / ${totalMem}GB\`\n` +
                      `🔋 *Node:* \`${process.version}\`\n\n` +
-                     `_Firebox Bot is a high-performance bot designed for professional group management._`;
+                     `_Kingred Bot is a high-performance bot designed for professional group management._`;
         
         await sock.sendMessage(jid, { text: info });
     }
